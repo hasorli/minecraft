72032d2ad554e1b3d53a598fc91cd15d1f765fbd
zleap <zleap@zleap.net>
2013-05-21 11:04:15 +0100

    minecraft

--- blockline2.py
+++ blockline2.py
@@ -4,10 +4,9 @@ import block
 mc = minecraft.Minecraft.create()
 
 x = raw_input("Block required ?")
-x = int(x)
-
+int (x)
 #block.BLOCK = float(BLOCK)
 
 for x in xrange(0, 10):
-  mc.setBlock(x, 13, 0, block.Block(x))
+  mc.setBlock(x, 10, 0, block.x)
    
